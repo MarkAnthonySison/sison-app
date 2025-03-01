95324fd92ec9fa1561f5f5e2160b639025b12f0f
unknown <markasison101503@gmail.com>
2025-03-01 18:14:59 +0800

    Exercise #3 - Routing , Middleware, CSRF

--- routes/web.php
+++ routes/web.php
@@ -1,29 +1,67 @@
 <?php
-use App\Services\UserService;
-use Illuminate\Http\Request;
+
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\ServiceProvider;
+use Illuminate\Http\Request;
+use App\Services\UserService;
 use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Response;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//Service Container
-Route::get('/test-container', function (Request $request ){
-    $input=$request->input ('key');
+Route::get('/test-container', function (Request $request) {
+    $input = $request->input('key');
     return $input;
 });
-//
-Route::get('/test-provider', function (UserService $userService ){
-    return $userService->listusers();
+
+Route::get('/test-provider', function (UserService $userService) {
+    return $userService->listUsers();
 });
 
-Route::get('/test-users', [UserController::class,'index']);
+Route::get('/test-users', [UserController::class, 'index']);
 
-Route::get('/test-facade',function (UserService $userService){
-    return Response::json($userService->listusers());
+Route::get('/test-facade', function(UserService $userService){
+    return Response::json($userService->listUsers());
 });
 
+Route::get('/post/{post}/comment/{comment}',function (string $postId, string $comment){
+    return "Post ID:" . $postId . "- Comment: ". $comment;
+});
 
+Route::get('/post/{id}', function (string $id) {
+    return $id;
+})->where('id', '[0-9]+');
 
+Route::get('/search/{search}', function (string $search) {
+    return $search;
+})->where('search', '.*');
+
+//Named Route
+Route::get('/test/route', function () {
+    return route ('test-route');
+})->name('test-route');
+
+Route::middleware(['user-middleware'])->group(function () {
+    Route::get('route-middleware-group/first', function (Request $request) {
+        echo 'first';
+    });
+
+    Route::get('route-middleware-group/second', function (Request $request) {
+        echo 'second';
+    });
+});
+
+Route::controller(UserController::class)->group(function (){
+    Route::get('/users', 'index');
+    Route::get('/users/first', 'first');
+    Route::get('/users/{id}', 'show');
+});
+
+Route::get('/token', function(Request $request) {
+    return view('token');
+});
+
+Route::post('/token', function(Request $request) {
+    return $request->all();
+});
